9563ae7458275a71853e597a8e6386501966b8c7
Luis Argumedo <huicho.argumedo@gmail.com>
2024-08-16 21:42:50 -0600

    idk

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.static("/images"));
 app.post("/profile", upload.single("avatar"), function (req, res, next) {
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
+  console.info(req.file);
 });
 
 // Use the router for handling routes
