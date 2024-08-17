02c54331cc264a337b2457dd7e44a16623c0f0ff
Luis Argumedo <huicho.argumedo@gmail.com>
2024-08-16 21:37:09 -0600

    multer

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
+const express = require("express");
+const path = require("path");
+const multer = require("multer");
+const indexRouter = require("./routes/index");
 
 const app = express();
 const PORT = 3000;
+const upload = multer({ dest: "images/" });
 
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("/images"));
+
+app.post("/profile", upload.single("avatar"), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+});
 
 // Use the router for handling routes
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
 // Catch-all route for handling 404 errors
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
